13abda5c8f847f6a9b7c6b01d23bf0ce65318216
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-09 16:42:01 -0400

    got event listener to work to pull content from button into calc window

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 ( function () {
   'use strict';
 
+  var elements = document.querySelectorAll('.cal-button');
 
-  // var one = getElementById('one').value;
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function(event){
+      var value = event.target.textContent;
+        var holdingNode = document.querySelector('.holding-math');
+        holdingNode.textContent = value;
+        console.log(value);
+        // try not even taking the content out of the element.. just put the element value in manually since you have to onclick for every div anyways
+
+      // console.log('event', event.target.textContent);
+      // console.log('clicked me');
+    })
+  }
+  // var one = getElementById('one').value;f
   // var two = getElementById('two').value;
   // var three = getElementById('three').value;
   // var four = getElementById('four').value;
@@ -23,7 +36,21 @@
   // var clear = getElementById('clear').value;
 
 
-// function to on click assign the value of the element clicked to variable one and create p tag in html that will hold it all and do a text.content to push that value into the p tag
+// function to onclick assign the value of the element clicked to variable value and create p tag in html that will hold it all and do a text.content to push that value into the p tag
+
+// so on click it will push the id information of the tag into function put in holding (id)
+
+// getElementById('id').textContent pulls all content from the tag
+// target.addEventListener('click', putInHolding);
+
+// var value = document.getElementById('seven');
+
+// function putInHolding() {
+//   var valueNode = document.getElementById(id).textContent;
+//   document.querySelector('.holding-math').textContent = valueNode;
+//   // try not even taking the content out of the element.. just put the element value in manually since you have to onclick for every div anyways
+//
+// }
 
 // then make it repeatable for every time a button is clicked
 
@@ -39,36 +66,36 @@
 // var op = '+';
 // alert(operators[op](10, 20));
 
-  function
+  // function
 
 
 
 // do math based on operation
 
-  function doMath(num1, num2) {
-    var num1 = window.prompt('pick first number');
-    console.log(num1);
-    var operator = window.prompt('pick operator');
-    console.log(operator);
-    var num2 = window.prompt('pick second number');
+  // function doMath(num1, num2) {
+  //   var num1 = window.prompt('pick first number');
+  //   console.log(num1);
+  //   var operator = window.prompt('pick operator');
+  //   console.log(operator);
+  //   var num2 = window.prompt('pick second number');
+  //
+  //   if (operator === 'add') {
+  //     return parseInt(num1) + parseInt(num2);
+  //   } else if (operator === 'multiply') {
+  //     return parseInt(num1) * parseInt(num2);
+  //   } else if (operator === 'subtract') {
+  //     return parseInt(num1) - parseInt(num2);
+  //   } else if (operator === 'divide') {
+  //     return parseInt(num1) / parseInt(num2);
+  //   }
+  //
+  //   }
+  //   var total = parseInt(num1) + operator + parseInt(num2);
+  //
+  // }
 
-    if (operator === 'add') {
-      return parseInt(num1) + parseInt(num2);
-    } else if (operator === 'multiply') {
-      return parseInt(num1) * parseInt(num2);
-    } else if (operator === 'subtract') {
-      return parseInt(num1) - parseInt(num2);
-    } else if (operator === 'divide') {
-      return parseInt(num1) / parseInt(num2);
-    }
 
-    }
-    var total = parseInt(num1) + operator + parseInt(num2);
-
-  }
-
-
-
+console.log('here i am working');
 
 
 })();
