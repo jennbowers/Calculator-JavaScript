0c5d1d351d4c3924f4839bc69cd773ebd307fa29
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-09 14:20:41 -0400

    created variable to pull values from all divs

--- main.js
+++ main.js
@@ -2,7 +2,31 @@
   'use strict';
   //all code goes here
 
-  
+  var one = getElementById('one').value;
+  var two = getElementById('two').value;
+  var three = getElementById('three').value;
+  var four = getElementById('four').value;
+  var five = getElementById('five').value;
+  var six = getElementById('six').value;
+  var seven = getElementById('seven').value;
+  var eight = getElementById('eight').value;
+  var nine = getElementById('nine').value;
+  var zero = getElementById('zero').value;
+  var add = getElementById('add').value;
+  var subtract = getElementById('subtract').value;
+  var multiply = getElementById('multiply').value;
+  var divide = getElementById('divide').value;
+  var decimal = getElementById('decimal').value;
+  var equals = getElementById('equals').value;
+  var clear = getElementById('clear').value;
+
+
+  // function doMath() {
+  //   var num1 = getElementById('id').value;
+  //   var operator = getElementById('id').value;
+  //   var num2 = getElementById('id').value;
+  //
+  //   var total = num1 + operator + num2;
 
   }
 
